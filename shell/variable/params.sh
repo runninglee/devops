6ec099b6357df00747a48dd2cc5ab8ab56f9c18d
Hui Lee <lihui870920@gmail.com>
2016-09-07 22:13:41 +0800

    SQL FILENAME

--- shell/variable/params.sh
+++ shell/variable/params.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
-# define params
 
-name="Crazy Lee"
+#变量名称声明等号之间不能有空格
+name="crazy lee"
 
-echo $name
+#输出变量使用双引号 饮用变量使用$
+echo "my name is $name"
 
-echo `date`
+#声明后缀的文件后缀
+so=".sql"
+
+#合并字符串 使用双引号引用变量
+filename=`date +%Y-%m-%d-%k-%M`"$so"
+
+#定义动态变量
+echo "$filename"gooo
